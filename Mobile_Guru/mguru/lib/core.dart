be0cf9aff4ddc7e295da11f733804c145ffa041e
Andis Trihariprasetya <andissetya@gmail.com>
2024-10-09 09:34:48 +0700

    Commit url core dan name package

--- Mobile_Guru/mguru/lib/core.dart
+++ Mobile_Guru/mguru/lib/core.dart
@@ -1,8 +1,6 @@
 class Core {
-  // String ApiUrl = "http://absensipolije.onklinik.net/Api/";
-  // String Url = "http://absensipolije.onklinik.net/";
-  String ApiUrl = "https://presensi.klinikdokterku.com/Api/";
-  String Url = "https://presensi.klinikdokterku.com/";
+  String ApiUrl = "https://presensi-smp1.esolusindo.com/Api/";
+  String Url = "https://presensi-smp1.esolusindo.com/";
   int Version = 35;
   double MaximalJarak = 100;
 }
