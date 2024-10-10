29a1c92f32616326739fc9898d345c5500793317
liztric <rendiardianto123.ra@gmail.com>
2024-10-10 10:36:51 +0700

    modified:   Presensi_Siswa/(Berhasil)nyobarai/lib/RecognitionScreen.dart 	modified:   Presensi_Siswa/(Berhasil)nyobarai/lib/RegistrationScreen.dart 	modified:   Presensi_Siswa/(Berhasil)nyobarai/lib/UserListScreen.dart 	modified:   Presensi_Siswa/(Berhasil)nyobarai/lib/main.dart 	modified:   Presensi_Siswa/(Berhasil)nyobarai/pubspec.lock 	modified:   Presensi_Siswa/(Berhasil)nyobarai/pubspec.yaml

--- Presensi_Siswa/(Berhasil)nyobarai/lib/main.dart
+++ Presensi_Siswa/(Berhasil)nyobarai/lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // late List<CameraDescription> cameras;
 // Future<void> main() async {
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
       title: 'Presensi SMP1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: const HomeScreen(),
       routes: {
