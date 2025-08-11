cd047b5636ab81c27bc7a44af1b5377296fb6830
fulayyy <159601192+fulayyy@users.noreply.github.com>
2025-08-11 14:28:29 +0700

    update

--- Presensi_Siswa/(Berhasil)nyobarai/test/widget_test.dart
+++ Presensi_Siswa/(Berhasil)nyobarai/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nyobarai/main.dart';
+import 'package:presensiSiswa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
