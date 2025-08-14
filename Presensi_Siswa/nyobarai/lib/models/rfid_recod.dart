01e276e8b49751192e095ddacbf751dd01703f95
fulayyy <159601192+fulayyy@users.noreply.github.com>
2025-08-14 14:43:18 +0700

    cupdate tampilan presensi siswa

--- Presensi_Siswa/nyobarai/lib/models/rfid_recod.dart
+++ Presensi_Siswa/nyobarai/lib/models/rfid_recod.dart
@@ -1,33 +1,33 @@
 class RfidRecord {
-  final String nis;
   final String nama;
   final String kelas;
+  final String nis;
   final String tanggal;
-  final String? jamMasuk;
+  final String? jamMasuk;   // dari 'waktu'
+  final String? jamPulang;  // dari 'waktu_pulang'
   final String statusMasuk;
-  final String? jamPulang;
   final String statusPulang;
 
   RfidRecord({
-    required this.nis,
     required this.nama,
     required this.kelas,
+    required this.nis,
     required this.tanggal,
     this.jamMasuk,
-    required this.statusMasuk,
     this.jamPulang,
+    required this.statusMasuk,
     required this.statusPulang,
   });
 
   factory RfidRecord.fromJson(Map<String, dynamic> json) {
     return RfidRecord(
-      nis: json['nis'] ?? '',
       nama: json['nama'] ?? '',
       kelas: json['kelas'] ?? '',
+      nis: json['nis'] ?? '',
       tanggal: json['tanggal'] ?? '',
-      jamMasuk: json['jam_masuk'],
+      jamMasuk: json['waktu'], // ambil masuk dari field 'waktu'
+      jamPulang: json['waktu_pulang'], // ambil pulang dari field 'waktu_pulang'
       statusMasuk: json['status_masuk'] ?? '',
-      jamPulang: json['jam_pulang'],
       statusPulang: json['status_pulang'] ?? '',
     );
   }
