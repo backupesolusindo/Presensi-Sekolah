2ce3b6469147d89638e534126c4dce1ccb6ea687
fulayyy <159601192+fulayyy@users.noreply.github.com>
2025-08-18 09:41:34 +0700

    up

--- Mobile_Guru/mguru_test/android/app/build.gradle.kts
+++ Mobile_Guru/mguru_test/android/app/build.gradle.kts
@@ -1,16 +1,10 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
     namespace = "com.example.mguru_test"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true // ✅ Kotlin DSL pakai "is"
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -28,17 +22,16 @@ android {
     }
 
     buildTypes {
+        // Release pakai debug keystore (boleh buat sementara biar gampang build)
         getByName("release") {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+
+        // Debug wajib eksplisit biar nggak ikut release
+        getByName("debug") {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0") 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
---
+++ Presensi_Siswa/nyobarai/android/app/src/main/kotlin/com/smpn1jember/nyobarai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.smpn1jember.nyobarai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
