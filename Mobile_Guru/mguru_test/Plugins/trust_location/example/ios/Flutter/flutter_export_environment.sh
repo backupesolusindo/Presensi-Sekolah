51bf477bbc1d341fcef98df891222cb8a3e43178
fulayyy <159601192+fulayyy@users.noreply.github.com>
2025-08-26 08:25:52 +0700

    giyas edit

--- Mobile_Guru/mguru_test/Plugins/trust_location/example/ios/Flutter/flutter_export_environment.sh
+++ Mobile_Guru/mguru_test/Plugins/trust_location/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Ghiyazt\Documents\baru-flutter-smt4\barubangetgaul\Presensi-Sekolah\Mobile_Guru\mguru_test\Plugins\trust_location\example"
+export "FLUTTER_APPLICATION_PATH=C:\Presensi-Sekolah\Mobile_Guru\mguru_test\Plugins\trust_location\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
